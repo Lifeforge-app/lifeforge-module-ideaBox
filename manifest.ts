e6e57dd7ad739e453a26e1cff5cd97bdad13e7e2
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 12:00:12 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,11 +2,8 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Idea Box',
-  icon: 'tabler:bulb',
   routes: {
     '/': lazy(() => import('@/pages/Containers')),
     '/:id/*': lazy(() => import('@/pages/Ideas'))
   },
-  category: 'Productivity'
 } satisfies ModuleConfig
